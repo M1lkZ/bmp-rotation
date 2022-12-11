ab53d04ebd3c576e1ff54db0ed2a55592380462a
M1lkZ <nickolasmarwin@gmail.com>
2022-12-11 19:32:42 +0300

    done

--- solution/src/main.c
+++ solution/src/main.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include "imageOP.h"
+#include "image.h"
+#include "generators.h"
+#include "picTransformer.h"
+
 
 int main( int argc, char** argv ) {
-    (void) argc; (void) argv; // supress 'unused parameters' warning
+    if (argc != 3){
+        fprintf(stderr, "Invalid arguments. Expected: path input.bmp output.bmp");
+        return -1;
+    }
+    char* file = argv[1];
+    char* rotated = argv[2];
+    FILE *src = fopen(file,"rb");
+    struct bmp_header header = read_header(src);
+    struct image orig = generate_pic(header.biHeight, header.biWidth);
+    struct image rot = generate_pic(header.biWidth, header.biHeight);
+    read_pixels(src, orig, header);
+    fclose(src);
 
+    transform(orig, rot);
+
+    FILE *dest = fopen(rotated, "wb");
+    write_img(dest, rot, generate_header(rot));
+    fclose(dest);
+    free(orig.data);
+    free(rot.data);
     return 0;
 }
